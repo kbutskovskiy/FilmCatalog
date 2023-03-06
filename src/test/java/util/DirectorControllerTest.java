cb6e4142a1c0920959c0756a39af406e1a751860
Kirill Buckovsky <kbutskovskiy@mail.ru>
2023-03-06 20:59:21 +0300

    dz16 v2

--- src/test/java/util/DirectorControllerTest.java
+++ src/test/java/util/DirectorControllerTest.java
@@ -3,20 +3,22 @@ package util;
 import com.example.catalogfilm.CatalogFilmApplication;
 import com.example.catalogfilm.model.Director;
 import com.example.catalogfilm.service.DirectorService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.UUID;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(classes = CatalogFilmApplication.class)
 @AutoConfigureMockMvc
@@ -28,25 +30,29 @@ public class DirectorControllerTest {
     private DirectorService directorService;
 
     @Test
-    public void getDirectorTest_whenMockMVC_thenReturnsHttpStatusOK() throws Exception {
-        mockMvc.perform(get("/director?directorUuid=4b60f5e7-5ec4-4d20-bf63-bd060f8097f9")).andDo(print())
-                .andExpect(status().isOk());
+    public void addDirectorTest_whenMockMVC_thenReturnHttpStatusOK() throws Exception {
+        Director director = new Director();
+        director.setName("Christofer Nolan");
+        when(directorService.saveDirector(any(Director.class))).thenReturn(director);
+        mockMvc.perform(MockMvcRequestBuilders.post("/director")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"Christofer Nolan\"}")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"name\":\"Christofer Nolan\"}"));
     }
 
     @Test
-    public void addDirectorTest_whenMockMVC_thenReturnHttpStatusOK() throws Exception {
-        Director director = new Director(UUID.fromString("4b60f5e7-5ec4-4d20-bf63-bd060f8097f9"), "Kirill", 21, "Russia", null);
-        mockMvc.perform(post("/director")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(asJsonString(director)))
-                .andExpect(status().isOk());
-    }
+    public void getDirectorTest_whenMockMVC_thenReturnsHttpStatusOK() throws Exception {
+        UUID directorUuid = UUID.randomUUID();
+        Director director = new Director();
+        director.setUuid(directorUuid);
+        director.setName("Christofer Nolan");
 
-    public static String asJsonString(final Director director) {
-        try {
-            return new ObjectMapper().writeValueAsString(director);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        when(directorService.getDirector(ArgumentMatchers.eq(directorUuid))).thenReturn(director);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/director")
+                        .param("directorUuid", directorUuid.toString())).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"uuid\":\"" + directorUuid + "\",\"name\":\"Christofer Nolan\"}"));
     }
 }
--- src/test/java/util/FilmControllerTest.java
+++ src/test/java/util/FilmControllerTest.java
@@ -1,23 +1,24 @@
 package util;
 
 import com.example.catalogfilm.CatalogFilmApplication;
-import com.example.catalogfilm.constants.GenreEnum;
 import com.example.catalogfilm.model.Film;
 import com.example.catalogfilm.service.FilmService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.UUID;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(classes = CatalogFilmApplication.class)
 @AutoConfigureMockMvc
@@ -26,28 +27,32 @@ public class FilmControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
-    private FilmService filmService;
-
-    @Test
-    public void getFilmTest_whenMockMVC_thenReturnsHttpStatusOK() throws Exception {
-        mockMvc.perform(get("/film?filmUuid=4b60f5e7-5ec4-4d20-bf63-bd060f8097f9")).andDo(print())
-                .andExpect(status().isOk());
-    }
+    private FilmService FilmService;
 
     @Test
     public void addFilmTest_whenMockMVC_thenReturnHttpStatusOK() throws Exception {
-        Film Film = new Film(UUID.fromString("4b60f5e7-5ec4-4d20-bf63-bd060f8097f9"), "KillTheBill", GenreEnum.HORROR, 8);
-        mockMvc.perform(post("/film")
+        Film film = new Film();
+        film.setTitle("TENET");
+        when(FilmService.saveFilm(any(Film.class))).thenReturn(film);
+        mockMvc.perform(MockMvcRequestBuilders.post("/film")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(asJsonString(Film)))
-                .andExpect(status().isOk());
+                        .content("{\"title\": \"TENET\"}")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"title\":\"TENET\"}"));
     }
 
-    public static String asJsonString(final Film Film) {
-        try {
-            return new ObjectMapper().writeValueAsString(Film);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    @Test
+    public void getFilmTest_whenMockMVC_thenReturnsHttpStatusOK() throws Exception {
+        UUID filmUuid = UUID.randomUUID();
+        Film film = new Film();
+        film.setUuid(filmUuid);
+        film.setTitle("TENET");
+
+        when(FilmService.getFilm(ArgumentMatchers.eq(filmUuid))).thenReturn(film);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/film")
+                        .param("filmUuid", filmUuid.toString())).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"uuid\":\"" + filmUuid + "\",\"title\":\"TENET\"}"));
     }
 }
